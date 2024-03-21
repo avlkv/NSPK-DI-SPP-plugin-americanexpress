be9ca8849f4a676e7d267ea9af730a38488b665e
Artyom <volkovartik@gmail.com>
2024-03-21 15:37:50 +0300

    edit text_content and conf

--- americanexpress.py
+++ americanexpress.py
@@ -4,12 +4,12 @@
 1/2 документ плагина
 """
 import logging
-import os
 import time
 from selenium.webdriver.common.by import By
 from src.spp.types import SPP_document
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.webdriver import WebDriver
 import dateparser
 from datetime import datetime
 import pytz
@@ -31,9 +31,9 @@ class AMERICANEXPRESS:
     HOST = "https://www.americanexpress.com/en-us/newsroom/all-news.html"
     _content_document: list[SPP_document]
     utc = pytz.UTC
-    date_begin = utc.localize(datetime(2023, 10, 1))
+    # date_begin = utc.localize(datetime(2023, 10, 1))
 
-    def __init__(self, webdriver, *args, **kwargs):
+    def __init__(self, webdriver: WebDriver, last_document: SPP_document = None, max_count_documents: int = 100, *args, **kwargs):
         """
         Конструктор класса парсера
 
@@ -45,6 +45,8 @@ class AMERICANEXPRESS:
 
         self.driver = webdriver
         self.wait = WebDriverWait(self.driver, timeout=20)
+        self.max_count_documents = max_count_documents
+        self.last_document = last_document
 
         # Логер должен подключаться так. Вся настройка лежит на платформе
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -59,8 +61,12 @@ class AMERICANEXPRESS:
         :rtype:
         """
         self.logger.debug("Parse process start")
-        self._parse()
-        self.logger.debug("Parse process finished")
+        try:
+            self._parse()
+        except Exception as e:
+            self.logger.debug(f'Parsing stopped with error: {e}')
+        else:
+            self.logger.debug("Parse process finished")
         return self._content_document
 
     def _parse(self, abstract=None):
@@ -75,8 +81,7 @@ class AMERICANEXPRESS:
         # ========================================
         # Тут должен находится блок кода, отвечающий за парсинг конкретного источника
         # -
-        self.driver.get(
-            "https://www.americanexpress.com/en-us/newsroom/all-news.html")  # Открыть страницу со списком RFC в браузере
+        self.driver.get(self.HOST)  # Открыть страницу со списком RFC в браузере
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.stack-2')))
 
         while len(self.driver.find_elements(By.XPATH,
@@ -93,36 +98,36 @@ class AMERICANEXPRESS:
                 time.sleep(uniform(0.1, 1.2))
                 self.driver.get(web_link)
                 self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.aem-container')))
-                text_content = self.driver.find_element(By.CLASS_NAME, 'aem-container').text
-                print(web_link)
-                print(title)
-                print(pub_date)
-                print(text_content)
-                print('-' * 45)
-                if pub_date < self.date_begin:
-                    self.logger.info(f"Достигнута дата раньше {self.date_begin}. Завершение...")
-                    break
-                document = SPP_document(
-                    None,
-                    title=title,
-                    abstract=abstract if abstract else None,
-                    text=text_content,
-                    web_link=web_link,
-                    local_link=None,
-                    other_data=None,
-                    pub_date=pub_date,
-                    load_date=None,
-                )
-                # Логирование найденного документа
-                self.logger.info(self._find_document_text_for_logger(document))
-                self._content_document.append(document)
+                text_content = self.driver.find_element(By.XPATH, "//*[contains(@class, 'newsroom-container')]").text
+                # print(web_link)
+                # print(title)
+                # print(pub_date)
+                # print(text_content)
+                # print('-' * 45)
+                # if pub_date < self.date_begin:
+                #     self.logger.info(f"Достигнута дата раньше {self.date_begin}. Завершение...")
+                #     break
+
+                other_data = None
+                doc = SPP_document(None,
+                                   title,
+                                   abstract,
+                                   text_content,
+                                   web_link,
+                                   None,
+                                   other_data,
+                                   pub_date,
+                                   datetime.now())
+
+                self.find_document(doc)
+
                 self.driver.close()
                 self.driver.switch_to.window(self.driver.window_handles[0])
             try:
                 self.driver.execute_script('arguments[0].click()', self.driver.find_element(By.XPATH,
                                                                                             '//span[contains(@class,\'btn\')]/span[contains(text(), \'Next\')]'))
             except:
-                self.logger.info('Не найдено перехода на след. страницу. Завершение...')
+                # self.logger.info('Не найдено перехода на след. страницу. Завершение...')
                 break
             self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.stack-2')))
             # print('=== NEW_PAGE ===')
@@ -143,3 +148,15 @@ class AMERICANEXPRESS:
         """
         return (f"Find document | name: {doc.title} | link to web: {doc.web_link} | publication date: {doc.pub_date} | "
                 f"text: {doc.text}")
+    def find_document(self, _doc: SPP_document):
+        """
+        Метод для обработки найденного документа источника
+        """
+        if self.last_document and self.last_document.hash == _doc.hash:
+            raise Exception(f"Find already existing document ({self.last_document})")
+
+        if self.max_count_documents and len(self._content_document) >= self.max_count_documents:
+            raise Exception(f"Max count articles reached ({self.max_count_documents})")
+
+        self._content_document.append(_doc)
+        self.logger.info(self._find_document_text_for_logger(_doc))
